e4812d7f7ac17723eafc42bc1ce3b610009122c1
艾儒 <ruidong.qrd@antgroup.com>
2024-08-22 19:26:22 +0800

    python format

--- dataproxy_sdk/python/dp_file_adapter.py
+++ dataproxy_sdk/python/dp_file_adapter.py
@@ -1,6 +1,7 @@
 from . import libdataproxy
 from . import dp_pb2 as proto
 
+
 class DataProxyFileAdapter:
     def __init__(self, config: proto.DataProxyConfig):
         self.data_proxy_file = libdataproxy.DataProxyFile(config.SerializeToString())
@@ -8,8 +9,16 @@ class DataProxyFileAdapter:
     def close(self):
         self.data_proxy_file.close()
 
-    def download_file(self, info: proto.DownloadInfo, file_path: str, file_format: proto.FileFormat):
-        self.data_proxy_file.download_file(info.SerializeToString(), file_path, file_format)
+    def download_file(
+        self, info: proto.DownloadInfo, file_path: str, file_format: proto.FileFormat
+    ):
+        self.data_proxy_file.download_file(
+            info.SerializeToString(), file_path, file_format
+        )
 
-    def upload_file(self, info: proto.UploadInfo, file_path: str, file_format: proto.FileFormat):
-        self.data_proxy_file.upload_file(info.SerializeToString(), file_path, file_format)
+    def upload_file(
+        self, info: proto.UploadInfo, file_path: str, file_format: proto.FileFormat
+    ):
+        self.data_proxy_file.upload_file(
+            info.SerializeToString(), file_path, file_format
+        )
