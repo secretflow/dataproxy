c518a71a5f0b17fe706f04272b961bdd0fb7bfc3
Joe <oeqqwq@live.com>
2024-08-26 14:52:05 +0800

    Add github actions (#3)
    * add github actions
    
    * add licenserc
    
    * python format
    
    * format
    
    * fix lincenserc
    
    * add copyright header
    
    * update template

--- dataproxy_sdk/python/dp_file_adapter.py
+++ dataproxy_sdk/python/dp_file_adapter.py
@@ -1,5 +1,21 @@
+# Copyright 2024 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from . import libdataproxy
 from . import dp_pb2 as proto
+
 
 class DataProxyFileAdapter:
     def __init__(self, config: proto.DataProxyConfig):
@@ -8,8 +24,16 @@ class DataProxyFileAdapter:
     def close(self):
         self.data_proxy_file.close()
 
-    def download_file(self, info: proto.DownloadInfo, file_path: str, file_format: proto.FileFormat):
-        self.data_proxy_file.download_file(info.SerializeToString(), file_path, file_format)
+    def download_file(
+        self, info: proto.DownloadInfo, file_path: str, file_format: proto.FileFormat
+    ):
+        self.data_proxy_file.download_file(
+            info.SerializeToString(), file_path, file_format
+        )
 
-    def upload_file(self, info: proto.UploadInfo, file_path: str, file_format: proto.FileFormat):
-        self.data_proxy_file.upload_file(info.SerializeToString(), file_path, file_format)
+    def upload_file(
+        self, info: proto.UploadInfo, file_path: str, file_format: proto.FileFormat
+    ):
+        self.data_proxy_file.upload_file(
+            info.SerializeToString(), file_path, file_format
+        )
