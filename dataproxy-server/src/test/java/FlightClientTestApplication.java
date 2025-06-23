b615a552834548651c8774dffef5cdcc06aedf1d
peter5232 <18832808069@163.com>
2025-06-23 20:13:04 +0800

    delete

--- dataproxy-server/src/test/java/FlightClientTestApplication.java
+++
@@ -1,6 +0,0 @@
-public class FlightClientTestApplication {
-    public static void main(String[] args) {
-        FlightClientTest flightClient = new FlightClientTest();
-        flightClient.testCommandDataMeshUpdate();
-    }
-}
